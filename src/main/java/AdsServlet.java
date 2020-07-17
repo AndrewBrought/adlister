4f6249de66834a4b23888ef207287a07b9c740a1
Andrew Brought <andrewbrought@gmail.com>
2020-07-17 09:57:20 -0500

    working on ads servlet

--- src/main/java/AdsServlet.java
+++ src/main/java/AdsServlet.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class AdsServlet {
+@WebServlet(name = "AdsServlet", urlPatterns = "/ads")
+public class AdsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+       Ads ListAdsDao = DaoFactory.getAdsDao();
+       String title = request.getParameter("title");
+       String description = request.getParameter("description");
+       Ad ad = new Ad(title, description);
+       ListAdsDao.all();
+
+    }
 }
--- src/main/webapp/ads/index.jsp
+++ src/main/webapp/ads/index.jsp
@@ -8,9 +8,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>$Title$</title>
+    <title>Ads Listing</title>
 </head>
 <body>
-$END$
+
 </body>
 </html>
